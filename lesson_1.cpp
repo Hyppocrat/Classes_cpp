99493b984129f1f2742829ef562bcf2888f08300
Hyppocrat <djsh92@mail.ru>
2019-03-29 12:50:03 +0300

    Create lesson_1.cpp

---
+++ lesson_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int* find_min(int *arr_begin, int *arr_end){
+    int *min_ptr = arr_begin;
+    while( arr_begin < arr_end ){
+        if(*min_ptr > *arr_begin){
+            min_ptr = arr_begin;
+        }
+        ++arr_begin;
+    }
+    return min_ptr;
+}
+
+int find_min_value(int *arr_begin, int *arr_end){
+    return *find_min(arr_begin, arr_end);
+}
+
+
+int main() {
+    int a[10] = {3,5,2,1,5,0,2,8,4,1};
+    int *mn = find_min(a+1, a+5);
+    cout << *mn << endl;
+    cout << mn - a<<endl;
+    return 0;
+}
